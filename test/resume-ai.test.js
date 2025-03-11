068a0c2849009fba46f553959284a12a4719b166
Jeet Prajapati <jeet.p@crestskillserve.com>
2025-03-11 17:37:44 +0530

    Initial commit

---
+++ test/resume-ai.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ResumeAi = require('../lib/resume-ai-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resume-ai-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ResumeAi.ResumeAiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
